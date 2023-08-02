5402ce5bda358fbd19eaeda945535ddf51458358
shiyigu <119463062+shiyigu@users.noreply.github.com>
2023-08-02 12:49:46 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,12 +28,17 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
+#add a text entry box and send the input to Fruityvice as part of the API call 
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 #define the new table and import the pandas package
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #display the table on the page
 streamlit.dataframe(fruityvice_normalized)
+
 
 
